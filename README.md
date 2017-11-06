afb5c8820312dfe1ddbfbe941eeb99a9622307cf
Joel Ross <joelross@uw.edu>
2017-11-05 21:00:27 -0800

    Add lecture starter code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Location
+
+This repository contains code examples demonstrating Android location services. See the accompanying [lecture notes](https://info448.github.io/location.html) for more details.
---
+++ app/src/main/java/edu/uw/locationdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package edu.uw.locationdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LOCATION";
+
+    private TextView textLat;
+    private TextView textLng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textLat = (TextView)findViewById(R.id.txt_lat);
+        textLng = (TextView)findViewById(R.id.txt_lng);
+
+
+    }
+
+
+}
